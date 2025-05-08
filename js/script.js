9611b0b1d81f8852c481ddfca12fa37da7918772
Steve Tamayo <141716251+stamayodesign@users.noreply.github.com>
2025-05-08 03:12:46 -0400

    Post Day 69 and other
    Posted Day 69 sketch and adjusted css and js to have image modal and be able to adjust updated information from the js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,65 @@
-
+const currentSketchProgress = 69;
+const currentCSPProgress = 2;
 const currentFullCardProgress = 2;
 const currentFullCardProgressText = "Current Full Card Progress: "+currentFullCardProgress+"%";
-const lastUpdateTime = "7 May 2025 Wednesday 20:22:30 EDT (UTC-04)"; //https://www.utctime.net/utc-to-edt-converter
+const lastUpdateTime = "7 May 2025 Thursday 03:11:21 EDT (UTC-04)"; //https://www.utctime.net/utc-to-edt-converter
+
+// Init Variables
+var modal;
+//var img;
+var modalImg;
+var captionText;
+var span;
+var sketchesOnPage;
+
+function addEventListenerFunc(){
+  if(sketchesOnPage.length > 0){
+    for (var i = 0; i<sketchesOnPage.length;i++){
+      sketchesOnPage[i].addEventListener("click", openModal);
+    }
+  }
+  //img.addEventListener("click", openModal);
+  span.addEventListener("click", closeModal);
+}
+
+function openModal(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+  console.log("//soro");
+}
+function closeModal(){
+  modal.style.display = "none";  
+}
+
+function onPageLoad(){
+  //Assign Variables
+  // Get the modal
+  modal = document.getElementById("pageModal");
+
+
+  sketchesOnPage = document.getElementsByClassName("sketchImage");  
+
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  //img = document.getElementById("sketchDay69");
+  modalImg = document.getElementById("img01");
+  captionText = document.getElementById("caption");
+
+  // Get the <span> element that closes the modal
+  span = document.getElementsByClassName("close")[0];
+
+  addEventListenerFunc();
+
+  updateFullCardProgressText();
+  updateLastUpdatedTime();
+}
 
 function updateFullCardProgressText(){
   document.getElementById("divCurrentFullCardProgressBar").style.width = (currentFullCardProgress+"%");
   document.getElementById("divCurrentFullCardProgressText").textContent = currentFullCardProgressText;
+  document.getElementById("tdCurrentProgressSketchText").textContent = currentSketchProgress;
+  document.getElementById("tdCurrentProgressCSPText").textContent = currentCSPProgress;
   console.log("//soro");
-  updateLastUpdatedTime();
 }
 
 function updateLastUpdatedTime(){
